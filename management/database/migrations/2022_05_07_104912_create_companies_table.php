34c483d7e902e67dca4969f7506130c479aa2065
mirette fayez khlil <rita.fayez21@gmail.com>
2022-05-07 13:59:07 +0200

    some tables

---
+++ management/database/migrations/2022_05_07_104912_create_companies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); 
+            $table->string('address');
+            $table->string('governorate');
+            $table->string('way to communicate');
+            // $table->string('5adem id');
+              // $table->string('app id');
+            $table->string('where to post');
+            $table->enum('commission', ['yes','no'])->default('no');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
---
+++ management/database/migrations/2022_05_07_112601_create_applications_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status', ['accepted','pending'])->default('pending');
+            $table->bigInteger('no_of_req_emp');
+            $table->dateTime('created_at', 0);
+            $table->string('notes');
+            $table->string('job');
+            $table->string('requirements');
+         
+            $table->float('salary', 8, 2);
+            $table->enum('insurance', ['yes','no'])->default('no');
+            $table->enum('transportation', ['yes','no'])->default('no');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+}
